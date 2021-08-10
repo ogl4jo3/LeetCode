fa7a3efc9c46f7f5710deaadf47f2d6bd8c99883
Willy <willy_zhan@mayohr.com>
2021-08-10 16:54:49 +0800

    PermutationsII

--- LeetCode/src/com/company/leetcode/todo/PermutationsII.java
+++ LeetCode/src/com/company/leetcode/todo/PermutationsII.java
@@ -1,5 +1,6 @@
 package com.company.leetcode.todo;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PermutationsII {
@@ -8,9 +9,31 @@ public class PermutationsII {
         // write your code here
         System.out.println("main:");
 
+        int[][] testNums = new int[][]{
+                {1, 1, 2},
+                {1, 2, 3},
+        };
+        int[][][] testAns = new int[][][]{
+                {{1, 1, 2}, {1, 2, 1}, {2, 1, 1}},
+                {{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}},
+        };
+        for (int i = 0; i < testNums.length; i++) {
+            System.out.println("================================================================");
+            System.out.println("i: " + i);
+            System.out.println("testNums: " + Arrays.toString(testNums[i]));
+            System.out.println("testAns: ");
+            for (int[] ans : testAns[i]) {
+                System.out.println(Arrays.toString(ans));
+            }
+            List<List<Integer>> act = permuteUnique(testNums[i]);
+            System.out.println("act: ");
+            for (List<Integer> a : act) {
+                System.out.println(Arrays.toString(a.toArray()));
+            }
+        }
     }
 
-    public List<List<Integer>> permuteUnique(int[] nums) {
+    public static List<List<Integer>> permuteUnique(int[] nums) {
         //TODO:
         return null;
     }
